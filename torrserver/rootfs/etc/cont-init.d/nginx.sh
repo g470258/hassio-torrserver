5aa5652eb3a682b41f716594951318917b4f124f
Gleb <61633115+g470258@users.noreply.github.com>
2023-07-06 11:38:08 +0300

    Add files via upload

---
+++ torrserver/rootfs/etc/cont-init.d/nginx.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/with-contenv bashio
+# ==============================================================================
+# Bas Nijholt's Hass.io Add-ons: torrserver
+# Configures NGINX for use with torrserver
+# ==============================================================================
+declare ingress_interface
+declare ingress_port
+
+ingress_port=$(bashio::addon.ingress_port)
+ingress_interface=$(bashio::addon.ip_address)
+sed -i "s/%%port%%/${ingress_port}/g" /etc/nginx/servers/ingress.conf
+sed -i "s/%%interface%%/${ingress_interface}/g" /etc/nginx/servers/ingress.conf
